03084294bf0dc83927d26f08cdbf3becc84fd43d
sparta597 <46186983+sparta597@users.noreply.github.com>
2020-11-07 10:59:05 +0800

    20-11-07 登录界面基本完成，滑动窗口开始

---
+++ app/src/main/java/com/example/logistics/MainFragment.java
@@ -0,0 +1,29 @@
+package com.example.logistics;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+    private String context;
+    private TextView mTextView;
+
+    public MainFragment(String context){
+        this.context = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment,container,false);
+        mTextView = (TextView)view.findViewById(R.id.txt_content);
+        mTextView.setText(context);
+        return view;
+    }
+}
